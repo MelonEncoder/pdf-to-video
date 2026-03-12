f10017b9c550f2e5ec58a64d7537595ad624519a
MelonEncoder <iangillette@proton.me>
2026-03-12 04:06:45 -0400

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Development environment for pdf-to-video";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            meson
+            ninja
+            pkg-config
+            poppler
+            opencv
+            ffmpeg
+          ];
+        };
+      }
+    );
+}
